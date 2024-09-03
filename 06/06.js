236542888d0d722f8f98fefa6b39e483b72af878
happy950430 <happy950430@gmail.com>
2024-09-03 16:49:13 +0900

    주사위JS코드

--- 06/06.js
+++ 06/06.js
@@ -1,14 +1,41 @@
-//dom 생성된 후 이벤트를 감지
 document.addEventListener('DOMContentLoaded', () => {
 
-  const bt1 = document.querySelector('button');
-  const img = document.querySelector('#msg > img');
+  //이미지 가져오기
+  const imgs = document.querySelectorAll('.dice > img');
+  console.log(imgs);
 
-  // 버튼의 이벤트 달기
-  bt1.addEventListener('click', () => {
-    let n= Math.floor(Math.random()*6)+1;//1~6
-    img.setAttribute('src', `../img/${n}.png`);
-    img.setAttribute('alt', `${n}`);
-  });
+  //버튼 가져오기
+  const bts = document.querySelectorAll('button');
+  console.log(bts);
 
+  //결과 가져오기
+  const msg = document.querySelector('#msg');
+  console.log(msg);
+
+  //버튼클릭 0~5번 주사위 for문 이용
+  // bts[0].addEventListener('click', () =>{});
+    for (let bt of bts){
+      // console.log(bt);
+      bt.addEventListener('click',() =>{
+        //컴퓨터 랜덤수
+        let comN = Math.floor(Math.random()*6) + 1; //1~6
+        imgs[0].setAttribute('src', `../img/${comN}.png`);//속성을바꿔줄때 setAttribute 사용
+        imgs[0].setAttribute('alt', `${comN}.png`);
+
+      //사용자 선택
+      console.log(bt.textContent.charAt(0));
+      let userN = parseInt(bt.textContent.charAt(0))//숫자를 문자데이터로 바꿔줌
+      imgs[1].setAttribute('src', `../img/${userN}.png`);
+      imgs[1].setAttribute('alt', `${userN}.png`);
+
+      //결과출력
+      if(comN === userN) {
+        msg.textContent = '맞음';
+      }
+      else {
+        msg.textContent = '틀림';
+      }
+      });
+    }
+  
 });
